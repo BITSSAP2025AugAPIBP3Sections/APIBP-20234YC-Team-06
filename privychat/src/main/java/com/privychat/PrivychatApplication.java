1ac866730b6406065dfc311b3f9717fcbe272c87
Mahaboobsab Mulla <emp.far1st@gmail.com>
2025-11-21 09:59:42 +0530

    Created SpringBoot Application

---
+++ privychat/src/main/java/com/privychat/PrivychatApplication.java
@@ -0,0 +1,13 @@
+package com.privychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivychatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivychatApplication.class, args);
+	}
+
+}
